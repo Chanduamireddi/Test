81f9cb9d0547e609dac212299e93edb09363ce48
Chanduamireddi <70947236+Chanduamireddi@users.noreply.github.com>
2021-07-28 09:53:37 +0530

    Added new print statement

--- TestProject/src/Test1.java
+++ TestProject/src/Test1.java
@@ -7,6 +7,7 @@ public class Test1 {
 	  System.out.println("Test Project2");
 	  System.out.println("Test Project3");
 	  System.out.println("Test Project4");
+	  System.out.println("Test Project5");
   }
 
 }
